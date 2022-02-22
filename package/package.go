288ea497637542349d4cd3e121470b886743dd6a
Jurgen <jurgen.kluft@gmail.com>
2022-02-22 20:12:54 +0800

    * add dependency on xjsmn * read layout

--- package/package.go
+++ package/package.go
@@ -5,29 +5,29 @@ import (
 	imgui "github.com/jurgen-kluft/libimgui/package"
 	xbase "github.com/jurgen-kluft/xbase/package"
 	"github.com/jurgen-kluft/xcode/denv"
-	xentry "github.com/jurgen-kluft/xentry/package"
+	xjsmn "github.com/jurgen-kluft/xjsmn/package"
 )
 
 // GetPackage returns the package object of 'qmk-keymap-wiz'
 func GetPackage() *denv.Package {
 	// Dependencies
-	xentrypkg := xentry.GetPackage()
 	xbasepkg := xbase.GetPackage()
+	xjsmnpkg := xjsmn.GetPackage()
 	imguipkg := imgui.GetPackage()
 	glfwpkg := glfw.GetPackage()
 
 	// The main (qmk-keymap-wiz) package
 	mainpkg := denv.NewPackage("qmk-keymap-wiz")
-	mainpkg.AddPackage(xentrypkg)
 	mainpkg.AddPackage(xbasepkg)
+	mainpkg.AddPackage(xjsmnpkg)
 	mainpkg.AddPackage(imguipkg)
 	mainpkg.AddPackage(glfwpkg)
 
 	// 'qmk-keymap-wiz' library
 	mainapp := denv.SetupDefaultCppAppProject("qmk-keymap-wiz", "github.com\\jurgen-kluft\\qmk-keymap-wiz")
 
-	mainapp.Dependencies = append(mainapp.Dependencies, xentrypkg.GetMainLib())
 	mainapp.Dependencies = append(mainapp.Dependencies, xbasepkg.GetMainLib())
+	mainapp.Dependencies = append(mainapp.Dependencies, xjsmnpkg.GetMainLib())
 	mainapp.Dependencies = append(mainapp.Dependencies, imguipkg.GetMainLib())
 	mainapp.Dependencies = append(mainapp.Dependencies, glfwpkg.GetMainLib())
 
