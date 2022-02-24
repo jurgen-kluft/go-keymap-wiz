6e10c63b62f61bc51ed2eb272cdd4f17c2df4424
Jurgen Kluft <jurgen@virtuosgames.com>
2022-02-24 16:49:09 +0800

    * added windows library dependencies * seeing how to render the keyboard in ImGui

--- package/package.go
+++ package/package.go
@@ -1,6 +1,7 @@
 package qmk_keymap_wiz
 
 import (
+	kler "github.com/jurgen-kluft/kle_reader/package"
 	glfw "github.com/jurgen-kluft/libglfw/package"
 	imgui "github.com/jurgen-kluft/libimgui/package"
 	xbase "github.com/jurgen-kluft/xbase/package"
@@ -15,6 +16,7 @@ func GetPackage() *denv.Package {
 	xjsmnpkg := xjsmn.GetPackage()
 	imguipkg := imgui.GetPackage()
 	glfwpkg := glfw.GetPackage()
+	klerpkg := kler.GetPackage()
 
 	// The main (qmk-keymap-wiz) package
 	mainpkg := denv.NewPackage("qmk-keymap-wiz")
@@ -22,6 +24,7 @@ func GetPackage() *denv.Package {
 	mainpkg.AddPackage(xjsmnpkg)
 	mainpkg.AddPackage(imguipkg)
 	mainpkg.AddPackage(glfwpkg)
+	mainpkg.AddPackage(klerpkg)
 
 	// 'qmk-keymap-wiz' library
 	mainapp := denv.SetupDefaultCppAppProject("qmk-keymap-wiz", "github.com\\jurgen-kluft\\qmk-keymap-wiz")
@@ -30,6 +33,11 @@ func GetPackage() *denv.Package {
 	mainapp.Dependencies = append(mainapp.Dependencies, xjsmnpkg.GetMainLib())
 	mainapp.Dependencies = append(mainapp.Dependencies, imguipkg.GetMainLib())
 	mainapp.Dependencies = append(mainapp.Dependencies, glfwpkg.GetMainLib())
+	mainapp.Dependencies = append(mainapp.Dependencies, klerpkg.GetMainLib())
+
+	if denv.OS == denv.OS_WINDOWS {
+		mainapp.AddLibrary("opengl32.lib;gdi32.lib;shell32.lib")
+	}
 
 	mainpkg.AddMainApp(mainapp)
 	return mainpkg
